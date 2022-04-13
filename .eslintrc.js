17200573f429b13f74f42ecef66ce1af17d5bf59
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-04-13 21:09:38 +0900

    fix: storybook 절대경로 사용
    #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@next/next/recommended',
-    'prettier',
-  ],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@next/next/recommended', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.eslint.json', './tsconfig.json'],
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
   rules: {
@@ -29,30 +20,23 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'import/extensions': 0,
     'import/prefer-default-export': 0,
-
     'react/function-component-definition': 0,
     'react/jsx-props-no-spreading': 0,
     'react/require-default-props': 0,
-
     // import order settings
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'builtin',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['react'],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
+    'import/order': ['error', {
+      groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+      pathGroups: [{
+        pattern: 'react',
+        group: 'builtin',
+        position: 'before'
+      }],
+      pathGroupsExcludedImportTypes: ['react'],
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
       },
-    ],
-  },
+      'newlines-between': 'always'
+    }]
+  }
 };
