8b0cb8d9e8dc4b5eecc42623f214f26afbd9cc21
yoon-junseo <ysj6844@naver.com>
2022-04-21 17:39:05 +0900

    feat: next.config.js에 설정 추가
    1. rewrites를 통해 proxy 사용
    #3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+    DESTINATION_URL: process.env.DESTINATION_URL,
+    SOURCE_PATH: process.env.SOURCE_PATH,
+  },
+  async rewrites() {
+    if (process.env.NODE_ENV !== 'production') {
+      return [
+        {
+          destination: process.env.DESTINATION_URL,
+          source: process.env.SOURCE_PATH,
+        },
+      ];
+    }
+  },
 };
