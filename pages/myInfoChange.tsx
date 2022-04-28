a243d9d372fcc6046ebc01fb80b30a6c7ebb6ce3
Park Sung Ho <qkrtjdgh97@gmail.com>
2022-04-29 01:10:51 +0900

    feat: myInfoChange page 추가
    #8

---
+++ pages/myInfoChange.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MyInfoChange from '@/components/MyInfoChange';
+
+import { Wrapper } from '@/styles/pages/LoginPage/style';
+
+const MyInfoChangePage = () => {
+  return (
+    <Wrapper>
+      <MyInfoChange />
+    </Wrapper>
+  );
+};
+
+export default MyInfoChangePage;
