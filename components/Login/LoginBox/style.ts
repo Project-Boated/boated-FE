89f9bd2c2b2a0584d3bcbad169b220e40ca0ae54
yoon-junseo <ysj6844@naver.com>
2022-04-28 22:20:32 +0900

    feat: LoginBox
    1. 로그인 페이지에 사용할 로그인 컴포넌트
    2. 스타일
    3. 스토리북
    #7

---
+++ components/Login/LoginBox/style.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+import Theme from '@/styles/Theme';
+
+export const Wrapper = styled.div`
+  position: relative;
+
+  width: 1003px;
+  height: 582px;
+
+  background: ${Theme.S_0};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 51px;
+`;
+
+export const SeaWaveWrapper = styled.div`
+  height: 0;
+
+  svg {
+    position: absolute;
+    top: -4px;
+    left: -3px;
+  }
+`;
+
+export const Container = styled.div`
+  position: absolute;
+  left: 600px;
+  top: 143px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: 339px;
+
+  & > :nth-child(3) {
+    margin: 91px 0 25px 0;
+  }
+`;
+
+export const Text = styled.span`
+  font-family: 'Gmarket Sans';
+
+  color: ${Theme.M_1};
+  font-size: 20px;
+  font-weight: 300;
+  line-height: 20px;
+
+  text-align: center;
+`;
