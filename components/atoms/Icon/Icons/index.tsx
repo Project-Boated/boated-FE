14b6f307868060a761f67de54579c1a6b9a5270b
yoon-junseo <ysj6844@naver.com>
2022-05-06 00:49:25 +0900

    feat: icon 추가
    1. SeaWaveMedium 추가
    #10

--- components/atoms/Icon/Icons/index.tsx
+++ components/atoms/Icon/Icons/index.tsx
@@ -2,4 +2,5 @@ export { default as BoatedSignature } from './BoatedSignature';
 export { default as BoatedSymbol } from './BoatedSymbol';
 export { default as ExclamationMark } from './ExclamationMark';
 export { default as SeaWaveLarge } from './SeaWaveLarge';
+export { default as SeaWaveMedium } from './SeaWaveMedium';
 export { default as KakaoSymbol } from './KaKaoSymbol';
