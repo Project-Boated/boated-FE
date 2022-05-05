25c9dad7e56c682d10bb652dc09f3fe3c2bfd3bc
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-05-05 20:03:09 +0900

    feat: user API 추가
    #8

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -1,4 +1,14 @@
 import client from '@/src/lib/api/client';
+import { User } from '@/src/lib/types/user';
+
+export const getMe = () => client.get('/api/account/profile').then((res) => res.data);
+
+export const getProfileImg = () => client.get('/api/account/profile/profile-image').then((res) => res.data);
+
+export const putProfileImg = (profileImg: FormData) =>
+  client.put('/api/account/profile/profile-image', { profileImg }).then((res) => res.data);
+
+export const patchProfile = (profile: User) => client.patch('/api/account/profile', profile).then((res) => res.data);
 
 export const isNicknameDuplicated = (nickname: string) =>
   client.post('/api/account/profile/nickname/unique-validation', { nickname }).then((res) => res.data);
