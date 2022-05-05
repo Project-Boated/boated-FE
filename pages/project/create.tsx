746a7580385a7a3f5018708c8a64fa11d9e16f40
yoon-junseo <ysj6844@naver.com>
2022-05-06 00:51:34 +0900

    feat: ProjectCreatePage
    #10

---
+++ pages/project/create.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import CreateBox from '@/components/project/CreateBox';
+
+const projectCreatePage = () => {
+  return <CreateBox />;
+};
+
+export default projectCreatePage;
