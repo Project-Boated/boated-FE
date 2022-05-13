ec831cd114136a49ebca248675f29b9c741f7e4b
yoon-junseo <ysj6844@naver.com>
2022-05-13 23:26:47 +0900

    feat: ProjectCreatePage
    1. 프로젝트 생성 페이지 완성
    #10

--- pages/project/create.tsx
+++ pages/project/create.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CreateBox from '@/components/project/CreateBox';
 
+import { Wrapper } from '@/styles/pages/Project/style';
+
+export interface RequiredInfoState {
+  name: string;
+  description: string;
+  [key: string]: string;
+}
+
+export interface OptionalInfoState {
+  year: string;
+  month: string;
+  date: string;
+  type: 'AM' | 'PM';
+  hour: string;
+  minute: string;
+}
+
 const projectCreatePage = () => {
-  return <CreateBox />;
+  const [isRight, setIsRight] = useState<boolean>(false);
+
+  const [requiredInfo, setRequiredInfo] = useState<RequiredInfoState>({
+    name: '',
+    description: '',
+  });
+
+  const [optionalInfo, setOptionalInfo] = useState<OptionalInfoState>({
+    year: '',
+    month: '',
+    date: '',
+    type: 'AM',
+    hour: '',
+    minute: '',
+  });
+
+  return (
+    <Wrapper>
+      <CreateBox
+        isRight={isRight}
+        requiredInfo={requiredInfo}
+        optionalInfo={optionalInfo}
+        setIsRight={setIsRight}
+        setRequiredInfo={setRequiredInfo}
+        setOptionalInfo={setOptionalInfo}
+      />
+    </Wrapper>
+  );
 };
 
 export default projectCreatePage;
