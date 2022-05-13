06326669188435015173da98f6efd30d9b04f1d2
yoon-junseo <ysj6844@naver.com>
2022-05-13 23:26:30 +0900

    feat: CreateBox
    1. 프로젝트 생성 컴포넌트 완성
    #10

--- components/project/CreateBox/index.tsx
+++ components/project/CreateBox/index.tsx
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { yearList, monthList, dayList, amHourList, pmHourList, minuteList } from '@/lib/constants/dropdownList';
+import { yearList, monthList, dateList, amHourList, pmHourList, minuteList } from '@/lib/constants/dropdownList';
 
 import Icon from '@/components/atoms/Icon';
 import Input from '@/components/atoms/Input';
 import Text from '@/components/atoms/Text';
+import Button from '@/components/atoms/Button';
 
 import DropDown from '@/components/common/DropDown';
+import ToggleSwitch from '@/components/common/ToggleSwitch';
 
 import Theme from '@/styles/Theme';
 
-import { Wrapper, SeaWaveWrapper, LeftContainer, RightContainer, StyledLabel } from './style';
+import { RequiredInfoState, OptionalInfoState } from '@/pages/project/create';
 
-const CreateBox = () => {
-  const [year, setYear] = useState<string>('');
+import {
+  Wrapper,
+  SeaWaveWrapper,
+  LeftContainer,
+  RightContainer,
+  WarningAbsoluteWrapper,
+  StyledLabel,
+  ProjectNameContainer,
+  ProjectDueDateContainer,
+  DateWrapper,
+  TimeWrapper,
+  ProjectDescriptionContainer,
+  StyledTextArea,
+} from './style';
 
-  const [projectName, setProjectName] = useState<string>('');
+interface Props {
+  isRight: boolean;
+  requiredInfo: RequiredInfoState;
+  optionalInfo: OptionalInfoState;
+  setIsRight: React.Dispatch<React.SetStateAction<boolean>>;
+  setRequiredInfo: React.Dispatch<React.SetStateAction<RequiredInfoState>>;
+  setOptionalInfo: React.Dispatch<React.SetStateAction<OptionalInfoState>>;
+}
 
-  const onChangeProjectName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setProjectName(e.target.value);
+const CreateBox = ({ isRight, requiredInfo, optionalInfo, setIsRight, setRequiredInfo, setOptionalInfo }: Props) => {
+  const onChangeRequiredInfo = (e: React.ChangeEvent<any>) => {
+    const { name, value } = e.target;
+
+    setRequiredInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   return (
@@ -32,17 +59,98 @@ const CreateBox = () => {
         <Icon icon="BoatedSymbol" width={49} height={46} />
         <span>프로젝트 생성하기</span>
       </LeftContainer>
+      <WarningAbsoluteWrapper>
+        <Text color={Theme.W_1} fontSize={12} fontWeight={400}>
+          *는 필수 입력사항이에요!
+        </Text>
+      </WarningAbsoluteWrapper>
       <RightContainer>
-        <div>
-          <StyledLabel htmlFor="projectName">
+        <ProjectNameContainer>
+          <StyledLabel htmlFor="name">
             프로젝트 이름 <span className="is-required">*</span>
           </StyledLabel>
-          <Input type="text" width={566} height={40} value={projectName} onChange={onChangeProjectName} />
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            width={566}
+            height={40}
+            value={requiredInfo.name}
+            onChange={onChangeRequiredInfo}
+          />
           <Text color={Theme.W_1} fontSize={8} fontWeight={400}>
             중복된 이름이에요!
           </Text>
-        </div>
-        <Text fontSize={14}>프로젝트 기한</Text>
+        </ProjectNameContainer>
+        <ProjectDueDateContainer>
+          <Text fontSize={14}>프로젝트 기한</Text>
+          <DateWrapper>
+            <DropDown
+              type="size-176"
+              defaultTitle="YYYY"
+              target="year"
+              selectList={yearList}
+              selectedItem={optionalInfo.year}
+              setSelectedItem={setOptionalInfo}
+            />
+            <DropDown
+              type="size-110"
+              defaultTitle="MM"
+              target="month"
+              selectList={monthList}
+              selectedItem={optionalInfo.month}
+              setSelectedItem={setOptionalInfo}
+            />
+            <DropDown
+              type="size-110"
+              defaultTitle="DD"
+              target="date"
+              selectList={dateList}
+              selectedItem={optionalInfo.date}
+              setSelectedItem={setOptionalInfo}
+            />
+          </DateWrapper>
+          <TimeWrapper>
+            <ToggleSwitch
+              width={134}
+              height={40}
+              leftContent="AM"
+              rightContent="PM"
+              isRight={isRight}
+              setIsRight={setIsRight}
+            />
+            <DropDown
+              type="size-72"
+              defaultTitle="HH"
+              target="hour"
+              selectList={isRight ? pmHourList : amHourList}
+              selectedItem={optionalInfo.hour}
+              setSelectedItem={setOptionalInfo}
+            />
+            <DropDown
+              type="size-72"
+              defaultTitle="MM"
+              target="minute"
+              selectList={minuteList}
+              selectedItem={optionalInfo.minute}
+              setSelectedItem={setOptionalInfo}
+            />
+          </TimeWrapper>
+        </ProjectDueDateContainer>
+        <ProjectDescriptionContainer>
+          <StyledLabel htmlFor="description">
+            프로젝트 소개 <span className="is-required">*</span>
+          </StyledLabel>
+          <StyledTextArea
+            id="description"
+            name="description"
+            value={requiredInfo.description}
+            onChange={onChangeRequiredInfo}
+          />
+        </ProjectDescriptionContainer>
+        <Button width={200} height={52}>
+          생성하기
+        </Button>
       </RightContainer>
     </Wrapper>
   );
--- components/project/CreateBox/style.ts
+++ components/project/CreateBox/style.ts
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
   display: flex;
 
   width: 1137px;
-  height: 582px;
+  height: 617px;
 
   background: ${Theme.S_0};
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
@@ -58,9 +58,27 @@ export const LeftContainer = styled.div`
 
 export const RightContainer = styled.div`
   width: 566px;
-  height: 416px;
+  height: 512px;
 
   margin-top: 69px;
+
+  z-index: 999;
+
+  & > :nth-child(2) {
+    margin: 28px 0 33px 0;
+  }
+
+  & > :last-child {
+    margin: 0 auto;
+    margin-top: 44px;
+  }
+`;
+
+export const WarningAbsoluteWrapper = styled.div`
+  position: absolute;
+
+  top: 12px;
+  right: 43px;
 `;
 
 export const StyledLabel = styled.label`
@@ -77,4 +95,56 @@ export const StyledLabel = styled.label`
 
 export const WarningWrapper = styled.div<{ hasError: boolean }>`
   visibility: ${({ hasError }) => (hasError ? 'visible' : 'hidden')};
+`;
+
+export const ProjectNameContainer = styled.div`
+  & > :nth-child(2) {
+    margin: 7px 0 5px 0;
+  }
+`;
+
+export const ProjectDueDateContainer = styled.div`
+  & > :nth-child(2) {
+    margin: 7px 0 15px 0;
+  }
+`;
+
+export const DateWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 428px;
+`;
+
+export const TimeWrapper = styled.div`
+  display: flex;
+
+  & > :first-child {
+    margin-right: 20px;
+  }
+
+  & > :nth-child(2) {
+    margin-right: 6px;
+  }
+`;
+
+export const ProjectDescriptionContainer = styled.div`
+  & > :nth-child(2) {
+    margin-top: 15px;
+  }
+`;
+
+export const StyledTextArea = styled.textarea`
+  width: 566px;
+  height: 108px;
+
+  border: none;
+  background: ${Theme.S_0};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+
+  outline: none;
+  resize: none;
+
+  padding: 7px 0 0 15px;
 `;
