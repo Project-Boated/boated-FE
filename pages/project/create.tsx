de6d997b5657a0211ea3c0f70ca5a172ea7bcc90
jun5e00 <ysj6844@naver.com>
2022-06-04 15:18:29 +0900

    feat: ProjectCreatePage에 api 요청 추가
    1. onClickSubmit 함수 생성
    #10

--- pages/project/create.tsx
+++ pages/project/create.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { useRouter } from 'next/router';
+
+import { createProject } from '@/lib/api/projects';
 
 import CreateBox from '@/components/project/CreateBox';
 
@@ -14,12 +17,14 @@ export interface OptionalInfoState {
   year: string;
   month: string;
   date: string;
-  type: 'AM' | 'PM';
+  type?: 'AM' | 'PM';
   hour: string;
   minute: string;
 }
 
 const projectCreatePage = () => {
+  const router = useRouter();
+
   const [isRight, setIsRight] = useState<boolean>(false);
 
   const [requiredInfo, setRequiredInfo] = useState<RequiredInfoState>({
@@ -36,6 +41,43 @@ const projectCreatePage = () => {
     minute: '',
   });
 
+  const onClickSubmit = useCallback(() => {
+    const { name, description } = requiredInfo;
+    const tempOptionalInfo = { ...optionalInfo };
+    const { year, month, date, hour, minute } = tempOptionalInfo;
+    delete tempOptionalInfo.type;
+
+    const tempOptionalInfoValues = Object.values(tempOptionalInfo);
+
+    if (name === '') {
+      alert('프로젝트 이름을 작성해주세요!');
+      return;
+    }
+
+    if (description === '') {
+      alert('프로젝트 소개를 작성해주세요!');
+      return;
+    }
+
+    if (!tempOptionalInfoValues.every((v) => v === '')) {
+      if (tempOptionalInfoValues.some((v) => v === '')) {
+        alert('프로젝트 기한 항목을 모두 채워주시거나 비워주시기 바랍니다!');
+        return;
+      }
+    }
+
+    createProject({
+      name,
+      description,
+      deadline: `${year}-${month}-${date}-${hour}-${minute}-00`,
+    })
+      .then((res) => {
+        alert('프로젝트가 생성되었습니다!');
+        router.push(`/project/${res.data.id}`);
+      })
+      .catch((e) => console.log(e));
+  }, [requiredInfo, optionalInfo]);
+
   return (
     <Wrapper>
       <CreateBox
@@ -45,6 +87,7 @@ const projectCreatePage = () => {
         setIsRight={setIsRight}
         setRequiredInfo={setRequiredInfo}
         setOptionalInfo={setOptionalInfo}
+        onClickSubmit={onClickSubmit}
       />
     </Wrapper>
   );
