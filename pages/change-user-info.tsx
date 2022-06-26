6d62b6acac409700678dd76786ad138995d1eb53
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-06-26 15:37:17 +0900

    feat: change-user-info page
    #19

--- pages/change-user-info.tsx
+++ pages/change-user-info.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
+import styled from 'styled-components';
 
 import AppLayoutSub from '@/components/common/AppLayoutSub';
 
 import ChangeUserInfo from '@/components/user/ChangeUserInfo';
+import ProfileNavigator from '@/components/user/ProfileNavigator';
+
+import { Wrapper } from '@/styles/pages/ChangeUserInfoPage/style';
 
 const ChangeUserInfoPage: NextPage = () => {
+  const [tabNumber, setTabNumber] = useState(0);
+
   return (
     <AppLayoutSub>
-      <ChangeUserInfo />
+      <Wrapper>
+        <ProfileNavigator tabNumber={tabNumber} setTabNumber={setTabNumber} />
+        {tabNumber === 0 && <ChangeUserInfo />}
+        {/* 초대 받은 프로젝트 1, 탈퇴하기 2 */}
+      </Wrapper>
     </AppLayoutSub>
   );
 };
