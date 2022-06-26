ed4d03dd1919e64f3a91f94dc9b255416780989c
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-06-26 15:36:24 +0900

    feat: GNB useGetMyInfo 사용
    #19

--- components/common/GNB/index.tsx
+++ components/common/GNB/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
 
+import useGetMyInfo from '@/hooks/useGetMyInfo';
+
 import Icon from '@/components/atoms/Icon';
 import Text from '@/components/atoms/Text';
 
@@ -13,10 +15,9 @@ import {
   Tab,
   Wrapper,
 } from '@/components/common/GNB/style';
-import useGetMyInfo from '@/hooks/useGetMyInfo';
 
 const GNB = () => {
-  const { data, isLoading } = useGetMyInfo();
+  const { data } = useGetMyInfo();
 
   return (
     <Wrapper>
@@ -28,17 +29,23 @@ const GNB = () => {
               <Icon icon="BoatedSignature" width={92} height={30} />
             </IconAnchor>
           </Link>
-          <Tab>
-            <Text fontSize={13}>Project</Text>
-          </Tab>
-          <Tab>
-            <Text fontSize={13}>배정된 Task</Text>
-          </Tab>
+          {data && (
+            <>
+              <Tab>
+                <Text fontSize={13}>Project</Text>
+              </Tab>
+              <Tab>
+                <Text fontSize={13}>배정된 Task</Text>
+              </Tab>
+            </>
+          )}
         </IconWrapper>
-        <ProfileWrapper>
-          <ProfileImg src={data.profileImageUrl} />
-          <Text fontSize={13}>{data.nickname}</Text>
-        </ProfileWrapper>
+        {data && (
+          <ProfileWrapper>
+            <ProfileImg src={data.profileImageUrl} />
+            <Text fontSize={13}>{data.nickname}</Text>
+          </ProfileWrapper>
+        )}
       </Container>
     </Wrapper>
   );
