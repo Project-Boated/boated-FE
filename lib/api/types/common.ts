dc31c37b74aee8856b953b113f3fa9b30781aacb
jun5e00 <ysj6844@naver.com>
2022-06-26 10:39:23 +0900

    feat: common type에 타입 추가
    1. Method에 GET 추가
    2. PersonInfoState
    #20

--- lib/api/types/common.ts
+++ lib/api/types/common.ts
@@ -1,6 +1,11 @@
-export type Method = 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
 export interface ApiResponse<T> {
   statusCode: number;
   data: T;
+}
+
+export interface PersonInfoState {
+  id: number;
+  nickname: string;
 }
