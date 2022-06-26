956e9c0e17225d0926a3e8630a8d0bbba73c5c07
jun5e00 <ysj6844@naver.com>
2022-06-26 10:41:50 +0900

    feat: projects API
    1. 내가 팀장인 프로젝트 목록 불러오기
    2. 내가 팀원인 프로젝트 목록 불러오기
    3. 내가 팀장인 종료된 프로젝트 목록 불러오기
    4. 내가 팀원인 종료된 프로젝트 목록 불러오기
    #20

--- lib/api/projects.ts
+++ lib/api/projects.ts
@@ -1,18 +1,35 @@
 import client from '@/lib/api/client';
+import { AxiosResponse } from 'axios';
 
 import request from './request';
 
-import { ProjectCreateRequestProps, ProjectCreateResponse } from './types';
+import { PostProjectRequestProps, PostProjectResponse, GetProjectsResponse } from './types';
 
 const projectsBaseUrl = '/api/projects';
 
 const projectsUrl = {
   projects: projectsBaseUrl,
+  projectsMyCaptain: `${projectsBaseUrl}/my/captain`,
+  projectsMyCaptainTerminated: `${projectsBaseUrl}/my/captain/terminated`,
+  projectsMyCrew: `${projectsBaseUrl}/my/crew`,
+  projectsMyCrewTerminated: `${projectsBaseUrl}/my/crew/terminated`,
 };
 
-export const createProject = ({ name, description, deadline }: ProjectCreateRequestProps) =>
-  request<ProjectCreateResponse, ProjectCreateRequestProps>('POST', projectsUrl.projects, {
+export const createProject = ({ name, description, deadline }: PostProjectRequestProps) =>
+  request<PostProjectResponse, PostProjectRequestProps>('POST', projectsUrl.projects, {
     name,
     description,
     deadline,
   });
+
+export const getProjectsMyCaptain = () =>
+  request<GetProjectsResponse>('GET', projectsUrl.projectsMyCaptain).then((res) => res.data.projects);
+
+export const getProjectsMyCaptainTerminated = () =>
+  request<GetProjectsResponse>('GET', projectsUrl.projectsMyCaptainTerminated).then((res) => res.data.projects);
+
+export const getProjectsMyCrew = () =>
+  request<GetProjectsResponse>('GET', projectsUrl.projectsMyCrew).then((res) => res.data.projects);
+
+export const getProjectsMyCrewTerminated = () =>
+  request<GetProjectsResponse>('GET', projectsUrl.projectsMyCrewTerminated).then((res) => res.data.projects);
