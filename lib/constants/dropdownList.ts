8837108aef94fc0e58e5523ac98b71a664b8f070
jun5e00 <ysj6844@naver.com>
2022-06-26 10:38:39 +0900

    feat: sortMethodList
    1. 정렬 리스트 추가
    #20

--- lib/constants/dropdownList.ts
+++ lib/constants/dropdownList.ts
@@ -9,3 +9,5 @@ export const amHourList = Array.from({ length: 12 }, (_, idx) => (idx < 10 ? `0$
 export const pmHourList = Array.from({ length: 12 }, (_, idx) => `${idx + 12}`);
 
 export const minuteList = Array.from({ length: 12 }, (_, idx) => (idx * 5 < 10 ? `0${idx * 5}` : `${idx * 5}`));
+
+export const sortMethodList = ['마감 기한 순', '가나다 순', 'Task 개수 순'];
