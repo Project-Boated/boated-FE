f696e96b9c4469bc2cbe82e8931802022ac3ae5c
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-07-07 00:21:17 +0900

    feat: Task 컴포넌트 WIP
    #19

---
+++ components/common/Task/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+
+import { TaskState } from '@/lib/api/types';
+
+import { Wrapper } from '@/components/common/Task/style';
+
+interface TaskProps {
+  index: number;
+  task: TaskState;
+}
+
+const Task = ({ index, task }: TaskProps) => {
+  return (
+    <Draggable draggableId={String(task.id)} index={index}>
+      {(provided) => (
+        <Wrapper {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+          {task.name}
+        </Wrapper>
+      )}
+    </Draggable>
+  );
+};
+
+export default Task;
---
+++ components/common/Task/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import Theme from '@/styles/Theme';
+
+export const Wrapper = styled.div`
+  min-width: 227px;
+  min-height: 186px;
+
+  background: ${Theme.S_0};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+
+  margin-top: 19px;
+`;
