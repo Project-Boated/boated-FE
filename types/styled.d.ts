ed9bed188d49b88f4359de5c2e9a42251df9be75
jun5e00 <ysj6844@naver.com>
2022-07-12 11:57:35 +0900

    feat: styled.d.ts 추가
    1. 코드 일관성을 위한 타입 선언 파일 작성
    #29

---
+++ types/styled.d.ts
@@ -0,0 +1,21 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    M_1: string;
+    M_2: string;
+
+    P_1: string;
+    P_2: string;
+    P_3: string;
+
+    S_0: string;
+    S_1: string;
+    S_2: string;
+    S_3: string;
+    S_4: string;
+    S_5: string;
+
+    W_1: string;
+  }
+}
