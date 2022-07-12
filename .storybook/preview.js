851ca1f74775cddf33e15cad05ac0416ad838aff
jun5e00 <ysj6844@naver.com>
2022-07-12 11:17:53 +0900

    feat: storybook에 react-query 추가
    1. preview.js에 react-query 설정 추가
    #29

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,13 @@
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 
 import { GlobalStyle } from '@/styles/GlobalStyle';
 import Theme from '@/styles/Theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -19,11 +24,11 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <ThemeProvider theme={Theme}>
         <Story />
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   ),
 ];
