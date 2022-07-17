ec31983f853d5fe2e78a22a43c12277fe9bcff75
jun5e00 <ysj6844@naver.com>
2022-07-17 15:05:07 +0900

    feat: project/SubInfo
    1. 프로젝트 부가 설명 박스
    2. 컴포넌트
    3. 스타일
    4. 스토리북
    #29

---
+++ components/project/SubInfo/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Icon from '@/components/atoms/Icon';
+
+import * as Styled from './style';
+
+export interface SubInfoProps {
+  children: React.ReactNode;
+}
+
+const SubInfo = ({ children }: SubInfoProps) => {
+  return (
+    <Styled.Container>
+      <Icon icon="BoatedSymbol" width={20} height={20} />
+      {children}
+    </Styled.Container>
+  );
+};
+
+export default SubInfo;
