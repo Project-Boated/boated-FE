9515b6ca5f50be784826c5d941576c47b17b2a36
jun5e00 <ysj6844@naver.com>
2022-07-17 15:00:12 +0900

    design: 공용 css 파일 생성
    1. boxDesign
    #29

---
+++ styles/common/index.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components';
+
+import Theme from '@/styles/Theme';
+
+export const boxDesign = css`
+  background: ${Theme.S_0};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+`;
