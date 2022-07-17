a57db77fcd106f01c05ef8205028e06b6570f88a
jun5e00 <ysj6844@naver.com>
2022-07-17 15:03:55 +0900

    feat: InfoTitle
    1. 컴포넌트
    2. 스타일
    3. 스토리북
    #29

---
+++ components/project/InfoTitle/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+
+import InfoTitle, { InfoTitleProps } from '@/components/project/InfoTitle';
+
+export default {
+  component: InfoTitle,
+  title: 'project/InfoTitle',
+};
+
+const Template: Story<InfoTitleProps> = (args: InfoTitleProps) => <InfoTitle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: '프로젝트 이름',
+  subTitle: '*255자 이내',
+};
