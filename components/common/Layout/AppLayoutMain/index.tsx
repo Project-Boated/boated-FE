9e244988bd5a77161064ef62fe45fe78fa54a3ca
jun5e00 <ysj6844@naver.com>
2022-07-17 14:48:35 +0900

    fix: AppLayoutMain
    1. 페이지별로 배경 이미지 위치가 다르기 때문에 height, bottom 추가
    #29

--- components/common/Layout/AppLayoutMain/index.tsx
+++ components/common/Layout/AppLayoutMain/index.tsx
@@ -7,12 +7,17 @@ import { Children } from '@/components/common/Layout/types';
 import { Wrapper, ChildrenContainer, BackgroundContainer } from '@/components/common/Layout/style';
 import { BackgroundSeaWave } from './style';
 
-const AppLayoutMain = ({ children }: Children) => {
+interface AppLayoutMainProps extends Children {
+  height?: string;
+  bottom?: string;
+}
+
+const AppLayoutMain = ({ height, bottom = '-35vh', children }: AppLayoutMainProps) => {
   return (
     <Wrapper>
       <GNB />
-      <ChildrenContainer height="100vh">{children}</ChildrenContainer>
-      <BackgroundContainer bottom="-35vh">
+      <ChildrenContainer height={height}>{children}</ChildrenContainer>
+      <BackgroundContainer bottom={bottom}>
         <BackgroundSeaWave />
       </BackgroundContainer>
     </Wrapper>
