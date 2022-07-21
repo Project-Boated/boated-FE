39674a299beefe093f8d581d3c3fcf0317f9d196
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-07-21 13:09:15 +0900

    fix: useModal 스크롤바 막기
    #19

--- hooks/useModal.tsx
+++ hooks/useModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 const useModal = () => {
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
@@ -8,6 +8,14 @@ const useModal = () => {
   const closeModal = useCallback(() => setIsShowModal(false), [setIsShowModal]);
 
   const toggleModal = useCallback(() => setIsShowModal((prev: boolean) => !prev), [setIsShowModal]);
+
+  useEffect(() => {
+    if (isShowModal) {
+      document.body.style.overflow = 'hidden';
+      return;
+    }
+    document.body.style.overflow = 'visible';
+  }, [isShowModal]);
 
   return { isShowModal, openModal, closeModal, toggleModal };
 };
