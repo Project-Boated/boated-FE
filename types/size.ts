2ce1928df25cc13027de567dd1b78f99f755ac73
jun5e00 <ysj6844@naver.com>
2022-07-31 12:11:42 +0900

    feat: types/size
    1. DropDownSize 추가
    2. LayoutProps 생성 -> 박스 디자인에 활용
    #32

--- types/size.ts
+++ types/size.ts
@@ -1,5 +1,5 @@
 export interface DropDownSize {
-  type: 'size-176' | 'size-110' | 'size-88' | 'size-72' | 'size-47';
+  type: 'size-357' | 'size-264' | 'size-176' | 'size-110' | 'size-88' | 'size-72' | 'size-47';
   borderRadius?: number;
 }
 
@@ -7,4 +7,18 @@ export interface SizeProps {
   width?: number;
   height?: number;
   borderRadius?: number;
+}
+
+export interface LayoutProps {
+  borderRadius?: number;
+  backgroundColor?: string;
+  boxShadow?: string;
+}
+
+export interface FontSizeProps {
+  fontSize?: number;
+  fontWeight?: number;
+  lineHeight?: number;
+  letterSpacing?: number;
+  color?: string;
 }
