9291b06cd87e94cc991de5f421ef50c34492834a
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-08-15 21:12:08 +0900

    feat: KanbanColumn Component Modal 추가, delete Icon 호버
    #33

--- components/project/Kanban/KanbanColumn/index.tsx
+++ components/project/Kanban/KanbanColumn/index.tsx
@@ -1,10 +1,14 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useState } from 'react';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 
 import { KanbanColumnState } from '@/lib/api/types';
 
+import useModal from '@/hooks/useModal';
+
 import Text from '@/components/atoms/Text';
 import Icon from '@/components/atoms/Icon';
+
+import KanbanColumnDeleteModal from '@/components/common/Modal/KanbanColumnDeleteModal';
 
 import Task from '@/components/project/Kanban/Task';
 
@@ -14,51 +18,57 @@ import * as Styled from './style';
 
 export interface KanbanColumnProps extends KanbanColumnState {
   index: number;
-  dataLength?: number;
+  kanbanDataLength?: number;
 }
 
-const KanbanColumn = ({ id, name, tasks, index, dataLength }: KanbanColumnProps) => {
-  const isCustomLane = useMemo(() => {
-    if (name === 'READY' || name === 'PROCESS' || name === 'CHECKING' || name === 'COMPLETE') {
-      return false;
-    }
-    return true;
-  }, [name]);
+const KanbanColumn = ({ id, name, tasks, index, kanbanDataLength }: KanbanColumnProps) => {
+  const [isIconVisible, setIsIconVisible] = useState(false);
+
+  const { isShowModal, closeModal, openModal } = useModal();
 
   return (
-    <Draggable draggableId={name} index={index}>
-      {(provided) => (
-        <Styled.Container {...provided.draggableProps} ref={provided.innerRef}>
-          <Styled.KanbanHeader {...provided.dragHandleProps}>
-            {dataLength === 4 && index === 3 && (
-              <Styled.IconWrapper>
-                <Icon icon="KanbanColumnAdd" />
-              </Styled.IconWrapper>
-            )}
-            {isCustomLane && (
-              <Styled.IconWrapper>
-                <Icon icon="KanbanColumnDelete" />
-              </Styled.IconWrapper>
-            )}
-            <Text fontSize={14} fontFamily={'Gmarket Sans'} color={Theme.S_0}>
-              {name}
-            </Text>
-          </Styled.KanbanHeader>
-          <Droppable droppableId={String(id)} type="task">
-            {(provided) => (
-              <Styled.KanbanContainer {...provided.droppableProps} ref={provided.innerRef}>
-                <>
-                  {tasks.map((task, index) => (
-                    <Task key={task.id} index={index} task={task} />
-                  ))}
-                  {provided.placeholder}
-                </>
-              </Styled.KanbanContainer>
-            )}
-          </Droppable>
-        </Styled.Container>
+    <>
+      {isShowModal && (
+        <KanbanColumnDeleteModal
+          kanbanLaneId={id}
+          columnName={name}
+          taskDataLength={tasks.length}
+          closeModal={closeModal}
+        />
       )}
-    </Draggable>
+      <Draggable draggableId={name} index={index}>
+        {(provided) => (
+          <Styled.Container {...provided.draggableProps} ref={provided.innerRef}>
+            <Styled.KanbanHeader
+              {...provided.dragHandleProps}
+              onMouseEnter={() => setIsIconVisible(true)}
+              onMouseLeave={() => setIsIconVisible(false)}
+            >
+              {isIconVisible && kanbanDataLength !== 1 && (
+                <Styled.IconWrapper onClick={openModal}>
+                  <Icon icon="KanbanColumnDelete" />
+                </Styled.IconWrapper>
+              )}
+              <Text fontSize={14} fontFamily={'Gmarket Sans'} color={Theme.S_0}>
+                {name}
+              </Text>
+            </Styled.KanbanHeader>
+            <Droppable droppableId={String(id)} type="task">
+              {(provided) => (
+                <Styled.KanbanContainer {...provided.droppableProps} ref={provided.innerRef}>
+                  <>
+                    {tasks.map((task, index) => (
+                      <Task key={task.id} index={index} task={task} />
+                    ))}
+                    {provided.placeholder}
+                  </>
+                </Styled.KanbanContainer>
+              )}
+            </Droppable>
+          </Styled.Container>
+        )}
+      </Draggable>
+    </>
   );
 };
 
