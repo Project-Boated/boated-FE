46f7713b883d101bd58149858dd1b6efea3da2dc
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-08-17 19:11:10 +0900

    feat: KanbanHeader name 수정 기능
    #33

--- components/project/Kanban/KanbanColumn/index.tsx
+++ components/project/Kanban/KanbanColumn/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
+import { useRouter } from 'next/router';
 
 import { KanbanColumnState } from '@/lib/api/types';
+import { putProjectsKanbanLaneName } from '@/lib/api/projects';
 
 import useModal from '@/hooks/useModal';
 
@@ -22,9 +24,61 @@ export interface KanbanColumnProps extends KanbanColumnState {
 }
 
 const KanbanColumn = ({ id, name, tasks, index, kanbanDataLength }: KanbanColumnProps) => {
-  const [isIconVisible, setIsIconVisible] = useState(false);
+  const router = useRouter();
+  const projectId = parseInt(router.query.id as string, 10);
 
   const { isShowModal, closeModal, openModal } = useModal();
+
+  const kanbanHeaderRef = useRef<HTMLDivElement>(null);
+  const kanbanHeaderInputRef = useRef<HTMLInputElement>(null);
+
+  const [changedHeaderName, setIsChangedHeaderName] = useState(name);
+  const [isIconVisible, setIsIconVisible] = useState(false);
+
+  const useDetectHeaderOutsideClick: any = (el: React.RefObject<HTMLDivElement>, initialState: boolean) => {
+    const [isActive, setIsActive] = useState(initialState);
+
+    useEffect(() => {
+      const pageClickEvent = async (e: Event) => {
+        if (el.current && !el.current.contains(e.target as Node)) {
+          setIsActive(!isActive);
+
+          // 기존 이름과 바뀐 이름이 다르면 API 요청
+          if (name !== changedHeaderName) {
+            await putProjectsKanbanLaneName({ projectId, kanbanLaneId: id, name: changedHeaderName });
+          }
+        }
+      };
+
+      if (isActive) {
+        window.addEventListener('click', pageClickEvent);
+      }
+
+      return () => {
+        window.removeEventListener('click', pageClickEvent);
+      };
+    }, [isActive, el, changedHeaderName]);
+
+    return [isActive, setIsActive];
+  };
+
+  const [isEditable, setIsEditable] = useDetectHeaderOutsideClick(kanbanHeaderRef, false);
+
+  const onChangeHeaderName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChangedHeaderName(e.target.value);
+  };
+
+  const onDoubleClickHeaderName = () => {
+    setIsEditable(true);
+    setIsIconVisible(false);
+  };
+
+  // current가 있을때 focus
+  useEffect(() => {
+    if (isEditable && kanbanHeaderInputRef.current) {
+      kanbanHeaderInputRef.current.focus();
+    }
+  }, [isEditable]);
 
   return (
     <>
@@ -40,18 +94,34 @@ const KanbanColumn = ({ id, name, tasks, index, kanbanDataLength }: KanbanColumn
         {(provided) => (
           <Styled.Container {...provided.draggableProps} ref={provided.innerRef}>
             <Styled.KanbanHeader
+              ref={kanbanHeaderRef}
               {...provided.dragHandleProps}
-              onMouseEnter={() => setIsIconVisible(true)}
+              onMouseEnter={() => isEditable || setIsIconVisible(true)}
               onMouseLeave={() => setIsIconVisible(false)}
+              onDoubleClick={onDoubleClickHeaderName}
             >
               {isIconVisible && kanbanDataLength !== 1 && (
                 <Styled.IconWrapper onClick={openModal}>
                   <Icon icon="KanbanColumnDelete" />
                 </Styled.IconWrapper>
               )}
-              <Text fontSize={14} fontFamily={'Gmarket Sans'} color={Theme.S_0}>
-                {name}
-              </Text>
+              {isEditable ? (
+                <Styled.HeaderChangeInput
+                  type="text"
+                  id="header-name"
+                  name="header-name"
+                  width={200}
+                  height={40}
+                  maxLength={15}
+                  value={changedHeaderName}
+                  onChange={onChangeHeaderName}
+                  ref={kanbanHeaderInputRef}
+                />
+              ) : (
+                <Text fontSize={14} fontFamily={'Gmarket Sans'} color={Theme.S_0}>
+                  {name}
+                </Text>
+              )}
             </Styled.KanbanHeader>
             <Droppable droppableId={String(id)} type="task">
               {(provided) => (
