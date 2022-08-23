1724b418e239ac6e5ad73af57872728b03cf9b42
jun5e00 <ysj6844@naver.com>
2022-08-23 22:54:08 +0900

    feat: useTimePicker
    1. 인자의 기본값 설정
    2. TimePicker에서 사용할 state, setState를 꺼내는 hook
    #39

---
+++ components/date/TimePicker/useTimePicker.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+import addZeroToNumber from '@/lib/util/addZeroToNumber';
+
+const today = new Date();
+
+const useTimePicker = (time: string = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`) => {
+  const [h, m] = time.split(':');
+  const AM_PM: 'AM' | 'PM' = +h < 12 ? 'AM' : 'PM';
+
+  const [hourType, setHourType] = useState<'AM' | 'PM'>(AM_PM);
+  const [hour, setHour] = useState<string>(addZeroToNumber(h));
+  const [minute, setMinute] = useState<string>(addZeroToNumber(m));
+
+  return { hourType, setHourType, hour, setHour, minute, setMinute };
+};
+
+export default useTimePicker;
