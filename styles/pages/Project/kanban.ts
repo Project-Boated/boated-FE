3bdcf658c207f03a78970c8f9f9e6722d8c9d77a
jun5e00 <ysj6844@naver.com>
2022-08-23 23:11:23 +0900

    feat: KanbanPage
    1. KanbanPage
    #39

---
+++ styles/pages/Project/kanban.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+
+  width: 1313px;
+
+  margin-top: 445px;
+
+  & > :nth-child(2) {
+    margin: 0 11px 0 15px;
+  }
+`;
+
+export const H1 = styled.h1`
+  font-size: 20px;
+  line-height: 28px;
+`;
