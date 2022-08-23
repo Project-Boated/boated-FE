6d44a905c6c368034e664bac71471957fcd7b8ae
jun5e00 <ysj6844@naver.com>
2022-08-23 22:59:36 +0900

    refactor: TimeTableBox
    1. Calendar에 props 전달
    2. storybook 수정
    #39

--- components/date/TimeTableBox/index.tsx
+++ components/date/TimeTableBox/index.tsx
@@ -1,47 +1,43 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import useModal from '@/hooks/useModal';
 
-import Calendar from '@/components/date/Calendar';
+import Calendar, { CalendarProps } from '@/components/date/Calendar';
 import TimeTableSelector from '@/components/date/TimeTableSelector';
-import TimePicker from '@/components/date/TimePicker';
-
-import { ProjectInfoState } from '@/lib/api/types';
+import TimePicker, { TimePickerProps } from '@/components/date/TimePicker';
 
 import * as Styled from './style';
 
-const TimeTableBox = ({ deadline }: Partial<Pick<ProjectInfoState, 'deadline'>>) => {
+export interface TimeTableBoxProps extends CalendarProps, Omit<TimePickerProps, 'isHourTypeAM'> {
+  year: string;
+  month: string;
+  hourType: 'AM' | 'PM';
+}
+
+const TimeTableBox = ({
+  year,
+  month,
+  date,
+  hourType,
+  hour,
+  minute,
+  setYear,
+  setMonth,
+  setDate,
+  setHourType,
+  setHour,
+  setMinute,
+}: TimeTableBoxProps) => {
   const { isShowModal: isShowCalendar, toggleModal: onClickCalendar } = useModal();
   const { isShowModal: isShowTimePicker, toggleModal: onClickTimePicker } = useModal();
-
-  let date,
-    tempYear,
-    tempMonth,
-    tempDay = '';
-  let time,
-    tempHour,
-    tempMinute,
-    tempSecond = '';
-  let tempHourType: 'AM' | 'PM' = 'AM';
-
-  if (deadline) {
-    [date, time] = deadline.split(' ');
-    [tempYear, tempMonth, tempDay] = date.split('-');
-    [tempHour, tempMinute, tempSecond] = time.split(':');
-    tempHourType = Number(tempHour) > 12 ? 'PM' : 'AM';
-  }
-
-  const [hourType, setHourType] = useState<'AM' | 'PM'>(tempHourType ?? 'AM');
-  const [hour, setHour] = useState<string>(tempHour ?? '00');
-  const [minute, setMinute] = useState<string>(tempMinute ?? '00');
 
   const isHourTypeAM: boolean = useMemo(() => hourType === 'AM', [hourType]);
 
   return (
     <Styled.Container>
       <Styled.SelectorContainer>
-        <TimeTableSelector type="Calendar" contents="2022.08.08" onClick={onClickCalendar} />
-        {isShowCalendar && <Calendar />}
+        <TimeTableSelector type="Calendar" contents={`${year}.${month}.${date}`} onClick={onClickCalendar} />
+        {isShowCalendar && <Calendar date={date} setYear={setYear} setMonth={setMonth} setDate={setDate} />}
       </Styled.SelectorContainer>
       <Styled.SelectorContainer>
         <TimeTableSelector type="Clock" contents={`${hour}:${minute} ${hourType}`} onClick={onClickTimePicker} />
