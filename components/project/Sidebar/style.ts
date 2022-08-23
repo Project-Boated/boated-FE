f8014cd1c8addc381e5837edbc6584f6cede4648
jun5e00 <ysj6844@naver.com>
2022-08-23 23:07:13 +0900

    refactor: Sidebar
    1. SideTab 추가
    2. href 객체 추가
    3. Calendar에 props 전달
    #39

--- components/project/Sidebar/style.ts
+++ components/project/Sidebar/style.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import Theme from '@/styles/Theme';
 import { boxDesign } from '@/styles/common';
 
 export const Container = styled.aside`
@@ -23,6 +24,22 @@ export const SideTab = styled.ul`
   height: 333px;
 
   ${boxDesign()}
+`;
+
+export const Tab = styled.li<{ isClicked: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: inherit;
+  height: 59px;
+
+  border-radius: 6px;
+  background-color: ${({ isClicked }) => (isClicked ? Theme.M_1 : Theme.S_0)};
+
+  font-size: 15px;
+  font-weight: 400;
+  color: ${({ isClicked }) => (isClicked ? Theme.S_0 : Theme.M_1)};
 `;
 
 export const CalendarWrapper = styled.div`
