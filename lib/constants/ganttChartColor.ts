ad4b4aabfeaa9f6857f6dfe9e636ba08e8f4fa34
jun5e00 <ysj6844@naver.com>
2022-09-15 17:27:01 +0900

    fix: ganttChartColor -> ganttChartColorList
    #45

--- lib/constants/ganttChartColor.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeOption } from '@/types/common';
-
-import Theme from '@/styles/Theme';
-
-const ganttChartColorOptionList: ThemeOption[] = Object.keys(Theme) as ThemeOption[];
-
-const ganttChartColor = ganttChartColorOptionList.map((key) => Theme[key]);
-
-export default ganttChartColor;
---
+++ lib/constants/ganttChartColorList.ts
@@ -0,0 +1,16 @@
+import { ThemeOption } from '@/types/common';
+
+const ganttChartColorList: ThemeOption[] = [
+  'P_1',
+  'P_6',
+  'P_2',
+  'P_3',
+  'M_2',
+  'P_7',
+  'P_8',
+  'P_9',
+  'P_10',
+  'P_11',
+] as ThemeOption[];
+
+export default ganttChartColorList;
