e51025e3e768bea87f7e1c1e1399700b527c969e
jun5e00 <ysj6844@naver.com>
2022-09-15 17:26:25 +0900

    feat: Icon
    1. GanttArrow -> 간트 차트에서 사용하는 task arrow
    #45

---
+++ components/atoms/Icon/Icons/GanttArrow.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GanttArrow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="39" height="36" viewBox="0 0 39 36" fill="none">
+      <path d="M1 0V31H34" stroke="#94B0DC" stroke-width="1.5" />
+      <rect x="30" y="31.2427" width="6" height="6" transform="rotate(-45 30 31.2427)" fill="#94B0DC" />
+    </svg>
+  );
+};
+
+export default GanttArrow;
