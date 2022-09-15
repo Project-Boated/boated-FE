cdbc7b868d67530c78b64bde739b97954b11e4c5
jun5e00 <ysj6844@naver.com>
2022-09-15 17:37:28 +0900

    feat: Calendar/Body
    #45

---
+++ components/ganttChart/Calendar/Body/index.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+
+import Body, { BodyProps } from '.';
+import useCalendar from '@/components/ganttChart/Calendar/useCalendar';
+
+export default {
+  component: Body,
+  title: 'ganttChart/Calendar/Body',
+};
+
+const Template: Story<BodyProps> = () => {
+  const { year, month, calendarList } = useCalendar();
+
+  return <Body year={year} month={month} height={500} calendarList={calendarList} projectList={[]} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
