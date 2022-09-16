c7ba8120a59cdf0b40836db8b164c3f7aa593782
jun5e00 <ysj6844@naver.com>
2022-09-16 16:00:49 +0900

    refactor: Sidebar
    1. Sidebar 하위 컴포넌트들 적용
    #58

--- components/project/Sidebar/index.tsx
+++ components/project/Sidebar/index.tsx
@@ -1,50 +1,24 @@
-import React, { useMemo } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import React from 'react';
 
 import useCalendar from '@/components/date/Calendar/useCalendar';
 import Calendar from '@/components/date/Calendar';
 
 import * as Styled from './style';
 
-const Sidebar = () => {
-  const router = useRouter();
+export interface SidebarProps {
+  ProjectInfo?: React.ReactNode;
+  UserInfo?: React.ReactNode;
+  TabList: React.ReactNode;
+}
+
+const Sidebar = ({ ProjectInfo, UserInfo, TabList }: SidebarProps) => {
   const { date, setDate, setMonth, setYear } = useCalendar();
-
-  const path = router.route.split('[id]')[1];
-  const {
-    query: { id },
-  } = router;
-
-  const sideTab = useMemo(
-    () => [
-      {
-        tab: '프로젝트&팀원 관리',
-        link: `/project/${id}`,
-        path: '',
-      },
-      {
-        tab: 'Task',
-        link: `/project/${id}/kanban`,
-        path: '/kanban',
-      },
-      { tab: '발표 영상', link: `/project/${id}/presentation`, path: '/presentation' },
-    ],
-    [],
-  );
 
   return (
     <Styled.Container>
       <Styled.ProjectProfileContainer />
-      <Styled.SideTab>
-        {Object.values(sideTab).map((info) => (
-          <Link href={info.link} key={info.tab}>
-            <a>
-              <Styled.Tab isClicked={info.path === path}>{info.tab}</Styled.Tab>
-            </a>
-          </Link>
-        ))}
-      </Styled.SideTab>
+      {ProjectInfo || UserInfo}
+      {TabList}
       <Calendar date={date} setDate={setDate} setMonth={setMonth} setYear={setYear} />
     </Styled.Container>
   );
