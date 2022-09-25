b561aedf6dfdfc7971d298ceff5dbf5c84ff2d23
jun5e00 <ysj6844@naver.com>
2022-09-25 21:13:36 +0900

    feat: KanbanPage
    1. router에서 id 꺼내서 사용
    2. Sidebar에 props 생성
    #60

--- pages/project/[id]/kanban.tsx
+++ pages/project/[id]/kanban.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import getSideTabList from '@/lib/util/getSideTabList';
 
 import AppLayoutMain from '@/components/common/Layout/AppLayoutMain';
 
 import Sidebar from '@/components/project/Sidebar';
+import SideTab from '@/components/project/Sidebar/TabList/SideTab';
 
 import Kanban from '@/components/project/Kanban';
 
 import * as Styled from '@/styles/pages/Project/kanban';
 
 const KanbanPage = () => {
+  const router = useRouter();
+
+  const [id, setId] = useState<number>();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    setId(parseInt(router.query.id as string, 10));
+  }, [router.isReady]);
+
   return (
     <AppLayoutMain height="100vh" bottom="-45vh">
-      <Styled.Container>
-        <Sidebar />
-        <Kanban />
-      </Styled.Container>
+      {id && (
+        <Styled.Container>
+          <Sidebar TabList={<SideTab path={router.asPath} sideTabList={getSideTabList(id)} />} />
+          <Kanban />
+        </Styled.Container>
+      )}
     </AppLayoutMain>
   );
 };
