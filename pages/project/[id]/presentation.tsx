fbc56b60c33300045b343d6711b23e5d339d96b8
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-10-08 17:10:04 +0900

    fix: presentation page sidebar 수정
    - #48

--- pages/project/[id]/presentation.tsx
+++ pages/project/[id]/presentation.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import getSideTabList from '@/lib/util/getSideTabList';
 
 import AppLayoutMain from '@/components/common/Layout/AppLayoutMain';
 
 import PresentationVideo from '@/components/project/PresentationVideo';
 import Sidebar from '@/components/project/Sidebar';
+import SideTab from '@/components/project/Sidebar/TabList/SideTab';
 
 import * as Styled from '@/styles/pages/Project/presentation';
 
 const PresentationPage = () => {
+  const router = useRouter();
+
+  const [id, setId] = useState<number>();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    setId(parseInt(router.query.id as string, 10));
+  }, [router.isReady]);
+
   return (
     <AppLayoutMain height="100vh" bottom="-45vh">
-      <Styled.Container>
-        <Sidebar />
-        <PresentationVideo />
-      </Styled.Container>
+      {id && (
+        <Styled.Container>
+          <Sidebar TabList={<SideTab path={router.asPath} sideTabList={getSideTabList(id)} />} />
+          <PresentationVideo />
+        </Styled.Container>
+      )}
     </AppLayoutMain>
   );
 };
