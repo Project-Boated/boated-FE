67fdfb90a0c82475d59f0c62f23591996f77e574
Sung Ho Park <qkrtjdgh97@gmail.com>
2022-10-13 17:00:54 +0900

    fix: 카카오 로그인 버튼 Link로 감싸기
    - #66

--- components/Login/KakaoLoginButton/index.tsx
+++ components/Login/KakaoLoginButton/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 import { kakaoLoginPath } from '@/lib/constants';
 
@@ -6,11 +7,14 @@ import Icon from '@/components/atoms/Icon';
 
 import { Wrapper } from './style';
 
-const KakaoLoginButton = () => (
-    <Wrapper href={kakaoLoginPath}>
-      <Icon icon="KakaoSymbol" width={21} height={20} />
-      <span>카카오 로그인</span>
-    </Wrapper>
+const KakaoLoginButton = () => {
+  return (
+    <Link href={kakaoLoginPath} passHref>
+      <Wrapper>
+        <Icon icon="KakaoSymbol" width={21} height={20} />
+        <span>카카오 로그인</span>
+      </Wrapper>
+    </Link>
   );
 
 export default KakaoLoginButton;
